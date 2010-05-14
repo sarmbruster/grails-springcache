7913acc20c924db8bb624af9f8df53068c78ea6b
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-05-14 14:33:10 +0100

    need grailsCentral repo

--- test/projects/springcache-test/grails-app/conf/BuildConfig.groovy
+++ test/projects/springcache-test/grails-app/conf/BuildConfig.groovy
@@ -1,25 +1,21 @@
-import grails.util.GrailsUtil
-
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
+grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolution = {
-    inherits "global"
-    log "warn"
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        mavenLocal()
-        mavenCentral()
-		if (GrailsUtil.grailsVersion =~ /^1\.3/) {
-			grailsCentral()
-		}
-        mavenRepo "http://snapshots.repository.codehaus.org"
-        mavenRepo "http://repository.codehaus.org"
-        mavenRepo "http://download.java.net/maven/2/"
-        mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-    }
+	inherits "global"
+	log "warn"
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+		mavenLocal()
+		mavenCentral()
+		mavenRepo "http://snapshots.repository.codehaus.org"
+		mavenRepo "http://repository.codehaus.org"
+		mavenRepo "http://download.java.net/maven/2/"
+		mavenRepo "http://repository.jboss.com/maven2/"
+	}
+	dependencies {
+	}
 
 }
