ee420cea98c3663d86e4006b5cf986ced9fa805a
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-06-03 22:43:42 +0100

    updated release notes, copyright & version

--- SpringcacheGrailsPlugin.groovy
+++ SpringcacheGrailsPlugin.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Rob Fletcher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator
 import org.slf4j.LoggerFactory
 import org.springframework.cache.ehcache.*
@@ -8,8 +23,9 @@ import org.springframework.web.filter.DelegatingFilterProxy
 import grails.plugin.springcache.web.key.DefaultKeyGenerator
 
 class SpringcacheGrailsPlugin {
+
 	def version = "1.2.1"
-	def grailsVersion = "1.2-M3 > *"
+	def grailsVersion = "1.2.0 > *"
 	def dependsOn = [:]
 	def pluginExcludes = [
 			"grails-app/views/**",
@@ -21,7 +37,7 @@ class SpringcacheGrailsPlugin {
 	def author = "Rob Fletcher"
 	def authorEmail = "rob@energizedwork.com"
 	def title = "Spring Cache Plugin"
-	def description = "Allows caching and flushing aspects to be added to Grails services using annotations."
+	def description = "Provides annotation-driven caching of service methods and page fragments."
 
 	def documentation = "http://grails.org/Springcache+Plugin"
 
