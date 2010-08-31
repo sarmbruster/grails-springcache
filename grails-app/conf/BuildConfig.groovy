a9bb99d84e85d8e4b8f61652e0561a230dd23641
Luke Daley <ld@ldaley.com>
2010-08-31 11:23:54 +1000

    whitespace.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,13 +1,13 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
+grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolution = {
 	inherits("global")
 	log "warn"
 	repositories {
-        grailsHome()
-        grailsPlugins()
+		grailsHome()
+		grailsPlugins()
 		mavenLocal()
 		mavenCentral()
 	}
