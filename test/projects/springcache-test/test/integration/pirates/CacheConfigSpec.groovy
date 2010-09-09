9bec6493a2ce00fa7459f9ea1816a9dc57bbd911
Rob Fletcher <rob@energizedwork.com>
2010-09-09 09:38:42 +0100

    spock-ified the integration tests in the test app

---
+++ test/projects/springcache-test/test/integration/pirates/CacheConfigSpec.groovy
@@ -0,0 +1,26 @@
+package pirates
+
+import grails.plugin.spock.IntegrationSpec
+import net.sf.ehcache.store.MemoryStoreEvictionPolicy
+
+class CacheConfigSpec extends IntegrationSpec {
+
+	def springcacheCacheManager
+
+	def "caches can be configured in app config"() {
+		when: "A cache that is configured in app config is retrieved from the cache manager"
+		def cache = springcacheCacheManager.getEhcache("configuredCache")
+
+		then: "The configuration should be correct"
+		cache.cacheConfiguration.timeToLiveSeconds == 86400L
+	}
+
+	def "cache defaults can be configured in app config"() {
+		when: "A cache that is configured in app config is retrieved from the cache manager"
+		def cache = springcacheCacheManager.getEhcache("configuredCache")
+
+		then: "Default cache properties should have been applied to the cache"
+		cache.cacheConfiguration.memoryStoreEvictionPolicy == MemoryStoreEvictionPolicy.LFU
+	}
+
+}
