6d2273da867dbb1b0505c2bd285746db8b90719c
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-09-11 02:07:10 +0100

    removed hamcrest dependency

--- test/projects/springcache-test/grails-app/conf/BuildConfig.groovy
+++ test/projects/springcache-test/grails-app/conf/BuildConfig.groovy
@@ -11,11 +11,9 @@ grails.project.dependency.resolution = {
 		mavenLocal()
 		mavenCentral()
 		mavenRepo "http://repository.codehaus.org/"
-		mavenRepo "http://m2repo.spockframework.org/ext/"
 	}
 	dependencies {
 		test "org.seleniumhq.selenium:selenium-firefox-driver:2.0a5"
-		test "org.hamcrest:hamcrest-all:1.2"
 		test("org.codehaus.groovy.modules.http-builder:http-builder:0.5.0") {
 			excludes "groovy"
 		}
