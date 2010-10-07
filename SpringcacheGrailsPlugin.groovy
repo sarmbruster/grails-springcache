401c50c26f722da689c61acb2d9a4fc5bcd6db74
Rob Fletcher <rob@energizedwork.com>
2010-10-07 10:00:49 +0100

    first cut of annotation for key generation strategy

--- SpringcacheGrailsPlugin.groovy
+++ SpringcacheGrailsPlugin.groovy
@@ -113,7 +113,6 @@ class SpringcacheGrailsPlugin {
 			springcacheFilter(GrailsFragmentCachingFilter) {
 				springcacheService = ref("springcacheService")
 				cacheManager = ref("springcacheCacheManager")
-				keyGenerator = new DefaultKeyGenerator()
 			}
 		}
 	}
--- test/projects/springcache-test/grails-app/controllers/musicstore/modules/LatestController.groovy
+++ test/projects/springcache-test/grails-app/controllers/musicstore/modules/LatestController.groovy
@@ -3,10 +3,13 @@ package musicstore.modules
 import musicstore.Album
 import grails.converters.*
 import grails.plugin.springcache.annotations.Cacheable
+import grails.plugin.springcache.web.key.MimeTypeAwareKeyGenerator
+import grails.plugin.springcache.annotations.CacheKeyStrategy
 
 class LatestController {
 
 	@Cacheable("latestControllerCache")
+	@CacheKeyStrategy(MimeTypeAwareKeyGenerator)
 	def albums = {
 		def albums = Album.list(sort: "dateCreated", order: "desc", max: 10)
 		withFormat {
