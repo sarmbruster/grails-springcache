d4c45aaa3a2695bc1cf4ab1b75008a9d25bb8f62
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-12-10 16:21:37 +0000

    new all-purpose key generator - no way to configure it via annotation yet

---
+++ src/groovy/grails/plugin/springcache/web/key/WebContentKeyGenerator.groovy
@@ -0,0 +1,39 @@
+package grails.plugin.springcache.web.key
+
+import grails.plugin.springcache.key.CacheKeyBuilder
+import grails.plugin.springcache.web.ContentCacheParameters
+
+class WebContentKeyGenerator extends DefaultKeyGenerator {
+
+	static final String X_REQUESTED_WITH = "X-Requested-With"
+
+	boolean ajax
+	boolean format
+	boolean method
+
+	@Override protected void generateKeyInternal(CacheKeyBuilder builder, ContentCacheParameters context) {
+		super.generateKeyInternal(builder, context)
+
+		if (ajax) handleAjax(builder, context)
+		if (format) handleFormat(builder, context)
+		if (method) handleMethod(builder, context)
+	}
+
+	void handleAjax(CacheKeyBuilder builder, ContentCacheParameters context) {
+		builder << "$X_REQUESTED_WITH: ${context.request.getHeader(X_REQUESTED_WITH)}"
+	}
+
+	void handleFormat(CacheKeyBuilder builder, ContentCacheParameters context) {
+		def contentFormat = context.request.format
+		if (contentFormat && contentFormat != "all") {
+			builder << "format: " << contentFormat
+		}
+	}
+
+	void handleMethod(CacheKeyBuilder builder, ContentCacheParameters context) {
+		def method = context.request.method
+		if (method in ["GET", "HEAD"]) method = "GET/HEAD"
+		builder << "method: $method"
+	}
+
+}
---
+++ test/unit/grails/plugin/springcache/web/key/WebContentKeyGeneratorSpec.groovy
@@ -0,0 +1,207 @@
+package grails.plugin.springcache.web.key
+
+import grails.plugin.spock.UnitSpec
+import grails.plugin.springcache.web.ContentCacheParameters
+import grails.util.GrailsWebUtil
+import javax.servlet.http.HttpServletRequest
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
+import static grails.plugin.springcache.web.key.WebContentKeyGenerator.X_REQUESTED_WITH
+import static org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.CONTENT_FORMAT
+
+class WebContentKeyGeneratorSpec extends UnitSpec {
+
+	def generator = new WebContentKeyGenerator()
+
+	def setup() {
+		registerMetaClass HttpServletRequest
+		HttpServletRequest.metaClass.getFormat = {->
+			delegate.getAttribute(CONTENT_FORMAT)
+		}
+	}
+
+	def "by default xhr header is ignored"() {
+		given:
+		def ajaxRequest = new RequestBuilder().headers((X_REQUESTED_WITH): "XMLHttpRequest").toCacheParams()
+		def nonAjaxRequest = new RequestBuilder().toCacheParams()
+
+		when:
+		def key1 = generator.generateKey(ajaxRequest)
+		def key2 = generator.generateKey(nonAjaxRequest)
+
+		then:
+		key1 == key2
+	}
+
+	def "by default format is ignored"() {
+		given:
+		def xmlRequest = new RequestBuilder().format("xml").toCacheParams()
+		def jsonRequest = new RequestBuilder().format("json").toCacheParams()
+
+		when:
+		def key1 = generator.generateKey(xmlRequest)
+		def key2 = generator.generateKey(jsonRequest)
+
+		then:
+		key1 == key2
+	}
+
+	def "by default method is ignored"() {
+		given:
+		def getRequest = new RequestBuilder().method("GET").toCacheParams()
+		def postRequest = new RequestBuilder().method("POST").toCacheParams()
+
+		when:
+		def key1 = generator.generateKey(getRequest)
+		def key2 = generator.generateKey(postRequest)
+
+		then:
+		key1 == key2
+	}
+
+	def "keys can differ depending on whether request is ajax or not"() {
+		given:
+		def ajaxRequest = new RequestBuilder().headers((X_REQUESTED_WITH): "XMLHttpRequest").toCacheParams()
+		def nonAjaxRequest = new RequestBuilder().toCacheParams()
+
+		and:
+		generator.ajax = true
+
+		when:
+		def key1 = generator.generateKey(ajaxRequest)
+		def key2 = generator.generateKey(ajaxRequest)
+		def key3 = generator.generateKey(nonAjaxRequest)
+
+		then:
+		key1 == key2
+		key1 != key3
+	}
+
+	def "keys can differ depending on request format"() {
+		given:
+		def xmlRequest = new RequestBuilder().format("xml").toCacheParams()
+		def jsonRequest = new RequestBuilder().format("json").toCacheParams()
+
+		and:
+		generator.format = true
+
+		when:
+		def key1 = generator.generateKey(xmlRequest)
+		def key2 = generator.generateKey(xmlRequest)
+		def key3 = generator.generateKey(jsonRequest)
+
+		then:
+		key1 == key2
+		key1 != key3
+	}
+
+	def "'all' format is ignored"() {
+		given:
+		def request1 = new RequestBuilder().format("all").toCacheParams()
+		def request2 = new RequestBuilder().toCacheParams()
+
+		and:
+		generator.format = true
+
+		when:
+		def key1 = generator.generateKey(request1)
+		def key2 = generator.generateKey(request2)
+
+		then:
+		key1 == key2
+	}
+
+	def "keys can differ depending on request method"() {
+		given:
+		def getRequest = new RequestBuilder().method("GET").toCacheParams()
+		def postRequest = new RequestBuilder().method("POST").toCacheParams()
+
+		and:
+		generator.method = true
+
+		when:
+		def key1 = generator.generateKey(getRequest)
+		def key2 = generator.generateKey(getRequest)
+		def key3 = generator.generateKey(postRequest)
+
+		then:
+		key1 == key2
+		key1 != key3
+	}
+
+	def "get and head requests should generate the same key"() {
+		given:
+		def getRequest = new RequestBuilder().method("GET").toCacheParams()
+		def headRequest = new RequestBuilder().method("HEAD").toCacheParams()
+
+		and:
+		generator.method = true
+
+		when:
+		def key1 = generator.generateKey(getRequest)
+		def key2 = generator.generateKey(headRequest)
+
+		then:
+		key1 == key2
+	}
+
+	static ContentCacheParameters cacheParams(String controllerName, String actionName, Map params = [:], String method = "GET", Map headers = [:]) {
+		def webRequest = GrailsWebUtil.bindMockWebRequest()
+		webRequest.controllerName = controllerName
+		webRequest.actionName = actionName
+		webRequest.currentRequest.parameters = params
+		webRequest.currentRequest.method = method
+		headers.each { k, v ->
+			webRequest.currentRequest.addHeader(k, v)
+		}
+		new ContentCacheParameters(webRequest)
+	}
+
+}
+
+class RequestBuilder {
+
+	private GrailsWebRequest webRequest
+
+	RequestBuilder() {
+		webRequest = GrailsWebUtil.bindMockWebRequest()
+		webRequest.controllerName = "a"
+		webRequest.actionName = "index"
+	}
+
+	RequestBuilder controller(String controllerName) {
+		webRequest.controllerName = controllerName
+		this
+	}
+
+	RequestBuilder action(String actionName) {
+		webRequest.actionName = actionName
+		this
+	}
+
+	RequestBuilder params(Map<String, String> params) {
+		webRequest.currentRequest.parameters = params
+		this
+	}
+
+	RequestBuilder method(String method) {
+		webRequest.currentRequest.method = method
+		this
+	}
+
+	RequestBuilder headers(Map<String, String> headers) {
+		headers.each { k, v ->
+			webRequest.currentRequest.addHeader(k, v)
+		}
+		this
+	}
+
+	RequestBuilder format(String format) {
+		webRequest.currentRequest.setAttribute(CONTENT_FORMAT, format)
+		this
+	}
+
+	ContentCacheParameters toCacheParams() {
+		new ContentCacheParameters(webRequest)
+	}
+
+}
