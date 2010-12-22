f267f3b8ecb8ab556f976b62b8e594d73b19324f
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-12-22 22:12:23 +0000

    documentation updates

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -6,9 +6,10 @@ grails {
 		copyright = "Copies of this document may be made for your own use and for distribution to others, provided that you do not charge any fee for such copies and further provided that each copy contains this Copyright Notice, whether distributed in print or electronically."
 		footer = "Developed by the <a href='http://gpc.github.com'>Grails Plugin Collective</a>"
 		
+		alias.cacheHeaders="4.4. Cache Headers"
 		alias.contentCacheKeys="4.5. Content Cache Keys"
 		alias.contentNegotiation="4.6. Content Negotiation"
-		alias.cacheSelection="4.9. Cache Selection"
-		alias.cacheConfiguration="6. Cache Configuration"
+		alias.cacheSelection="5. Cache Selection"
+		alias.cacheConfiguration="7. Cache Configuration"
 	}
 }
